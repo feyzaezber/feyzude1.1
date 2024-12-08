d9e2071710b06b3ca533ef25d93fbfe2c2702be4
Feyza <ezberfeyza@gmail.com>
2024-12-09 00:23:46 +0300

    admin paneli oluşturma
    Kuaför web sitesi

---
+++ Feyzude/Feyzude/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Feyzude.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
